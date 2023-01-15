4f76c59bf5dfa6174a8882e51adc93ae4090b514
unknown <4ndrey.tkachenko@gmail.com>
2023-01-15 21:42:37 +0200

    Update App.js

--- App.js
+++ App.js
@@ -9,10 +9,7 @@ import {
   View,
   TouchableOpacity,
 } from "react-native";
-// import Blank from "./Screens/screen";source={"../public/imgBgReg.png"}
-// const image = {require('./assets/imgBgReg.png')};
-// const image = {uri: "./assets/imgBgReg.png"};
-// const image = {uri: "https://reactjs.org/logo-og.png"};
+// import Blank from "./Screens/screen";
 
 export default function App() {
   const [login, setLogin] = useState("");
@@ -30,6 +27,7 @@ export default function App() {
         style={styles.image}
       >
         <View style={styles.formBox}>
+          <View style={styles.picBox}></View>
           <Text style={styles.title}>Регистрация</Text>
           <TextInput
             style={styles.input}
@@ -53,6 +51,7 @@ export default function App() {
             <Text style={styles.btnTitle}>Зарегистрироваться</Text>
           </TouchableOpacity>
           <Text>Уже есть аккаунт? Войти</Text>
+          <View style={styles.indicator}></View>
         </View>
       </ImageBackground>
     </View>
@@ -66,20 +65,33 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    justifyContent: "center",
+    // justifyContent: "center",
+    justifyContent: "end",
     alignItems: "center",
     // justifyContent: "center",
   },
   formBox: {
-    // flex: 1,
-    marginTop: 263,
+    position: "relative",
     paddingTop: 92,
     paddingBottom: 15,
     borderRadius: 26,
     alignItems: "center",
+
     backgroundColor: "#fff",
-    width: 375,
-    height: 549,
+    // width: 375,
+    width: "100%",
+    height: "67%",
+  },
+  picBox: {
+    position: "absolute",
+    top: -60,
+    width: 120,
+    height: 120,
+    borderColor: "#E8E8E8",
+    borderRadius: 16,
+    borderWidth: 1,
+    backgroundColor: "#F6F6F6",
+    // boxShadows: "10px 5px 5px 10px red",
   },
   title: {
     fontSize: 30,
@@ -94,13 +106,13 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderWidth: 1,
     backgroundColor: "#F6F6F6",
-    width: 343,
+    width: "91%",
     height: 50,
   },
   btn: {
     backgroundColor: "#FF6C00",
     height: 51,
-    width: 343,
+    width: "91%",
     borderRadius: 25,
     marginTop: 40,
     marginBottom: 16,
@@ -111,5 +123,11 @@ const styles = StyleSheet.create({
   btnTitle: {
     color: "#fff",
     fontSize: 18,
+  },
+  indicator: {
+    marginTop: 66,
+    width: 134,
+    height: 5,
+    backgroundColor: "#212121",
   },
 });
