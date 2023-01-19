cde701d164bea9970c22d36a48cf75afbae20407
unknown <4ndrey.tkachenko@gmail.com>
2023-01-19 23:40:53 +0200

    Create Home.jsx

---
+++ Screens/Home.jsx
@@ -0,0 +1,21 @@
+// Home.js
+
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Home;
