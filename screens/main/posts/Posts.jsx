1470fb085854095e91e57216342c56ed33ffdeff
unknown <4ndrey.tkachenko@gmail.com>
2023-01-25 00:02:11 +0200

    Update Posts.jsx

--- screens/main/posts/Posts.jsx
+++ screens/main/posts/Posts.jsx
@@ -15,9 +15,12 @@ const PostsScreen = () => {
           <Text style={styles.userEmail}>email@example.com</Text>
         </View>
       </View>
+      <View style={styles.photoContainer}>
+        <View style={styles.container}></View>
+        <Text>image</Text>
+      </View>
       <View style={styles.cont}>
         <Text>Posts Screen!</Text>
-        <Text>Posts Screen!!</Text>
       </View>
     </View>
   );
@@ -28,6 +31,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     alignItems: "center",
+  },
+  photoContainer: {
+    height: 240,
+    width: "100%",
+    marginTop: 32,
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
   },
   userBox: {
     padding: 8,
