4535b6f32641dad880fd8c9c2e3df4bb8ac2f791
Andrey Tkachenko <4ndrey.tkachenko@gmail.com>
2023-01-24 15:32:34 +0200

    Create routing.jsx

---
+++ routing.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+
+import LoginScreen from "./screens/auth/login";
+import RegisterScreen from "./screens/auth/regist";
+import PostsScreen from "./screens/main/posts/Posts";
+import CreateScreen from "./screens/main/createPost/CreatePosts";
+import ProfileScreen from "./screens/main/profile/Profile";
+
+const AuthStack = createStackNavigator();
+const MainTab = createBottomTabNavigator();
+
+export const useRoute = (isAuth) => {
+  if (!isAuth) {
+    return (
+      <AuthStack.Navigator>
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <AuthStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Register"
+          component={RegisterScreen}
+        />
+      </AuthStack.Navigator>
+    );
+  }
+  return (
+    <MainTab.Navigator tabBarOptions={{ showLabel: false }}>
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              name="postage-stamp"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+        name="Posts"
+        component={PostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <AntDesign name="pluscircleo" size={35} color={color} />
+          ),
+        }}
+        name="Create"
+        component={CreateScreen}
+      />
+      <MainTab.Screen
+        options={{
+          tabBarIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              name="face-profile"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
+    </MainTab.Navigator>
+  );
+};
