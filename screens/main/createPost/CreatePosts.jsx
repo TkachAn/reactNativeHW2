90298828eeef0aa990958ce4252cc498c6db2b4f
unknown <4ndrey.tkachenko@gmail.com>
2023-01-24 17:06:36 +0200

    Update CreatePosts.jsx

--- screens/main/createPost/CreatePosts.jsx
+++ screens/main/createPost/CreatePosts.jsx
@@ -3,7 +3,7 @@ import {View, Text, StyleSheet} from "react-native";
 const CreatePosts = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.img}>
+      {/* <View style={styles.img}>
        <Text>image Screen</Text>
       </View>
       <View style={styles.title}>
@@ -11,8 +11,8 @@ const CreatePosts = () => {
       </View>
       <View style={styles.map}>
        <Text>map Screen</Text>
-      </View>
-                      {/* <TouchableOpacity
+      </View> */}
+      {/* <TouchableOpacity
                   style={{...styles.btn, marginTop: isShowKeyboard ? 0 : 40}}
                   onPress={keyboardHide}
                   activeOpacity={0.8}
@@ -29,16 +29,16 @@ const styles = StyleSheet.create({
     padding: 16,
     alignItems: "center",
   },
-    img: {
-    width:320,
-    height:240,
+  img: {
+    width: 320,
+    height: 240,
     borderWidth: 1,
     borderColor: "#888",
     alignItems: "center",
     justifyContent: "center",
   },
-    title: {
-    width:"80%",
+  title: {
+    width: "80%",
     // alignItems: "center",
     justifyContent: "center",
   },
