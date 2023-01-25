844b5c3811a4c663ad7458469a65790761f8137a
unknown <4ndrey.tkachenko@gmail.com>
2023-01-25 23:35:23 +0200

    Update Posts.jsx

--- screens/main/posts/Posts.jsx
+++ screens/main/posts/Posts.jsx
@@ -1,89 +1,38 @@
 import React from "react";
-import {StyleSheet, Text, View, Image} from "react-native";
+// import { moduleName } from "react-native";
+import {createStackNavigator} from "@react-navigation/stack";
+import DefaultPosts from "../../nested/defaultPosts";
+import Comments from "../../nested/Comments";
+import Maps from "../../nested/Map";
+
+const Nested = createStackNavigator();
+
 const PostsScreen = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.userBox}>
-        <View style={styles.userPic}>
-          <Image
-            source={require("../../../assets/adaptive-icon.png")}
-            style={{width: 60, height: 60}}
-          />
-        </View>
-        <View style={styles.userText}>
-          <Text style={styles.userName}>Natali Romanoff</Text>
-          <Text style={styles.userEmail}>natali@romanoff.com</Text>
-        </View>
-      </View>
-      <View style={styles.photoContainer}>
-        <View style={styles.container}></View>
-        <Text>image</Text>
-      </View>
-      <View style={styles.cont}>
-        <Text>text</Text>
-      </View>
-    </View>
+    <Nested.Navigator>
+      <Nested.Screen
+        name="DefaultPosts"
+        component={DefaultPosts}
+        options={{
+          title: "Публикации",
+        }}
+      />
+      <Nested.Screen
+        name="Comments"
+        component={Comments}
+        options={{
+          title: "Комментарии",
+        }}
+      />
+      <Nested.Screen
+        name="Map"
+        component={Maps}
+        options={{
+          title: "Карта",
+        }}
+      />
+    </Nested.Navigator>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    alignItems: "center",
-  },
-  photoContainer: {
-    height: 240,
-    width: "100%",
-    marginTop: 32,
-    backgroundColor: "#F6F6F6",
-    borderWidth: 1,
-    borderColor: "#E8E8E8",
-    borderRadius: 8,
-  },
-  userBox: {
-    padding: 8,
-    marginBottom: 16,
-    backgroundColor: "#fff",
-    width: "100%",
-
-    flexDirection: "row",
-  },
-
-  userPic: {
-    width: 60,
-    height: 60,
-    // borderColor: "#888888",
-    borderRadius: 18,
-    // borderWidth: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#888",
-  },
-  userText: {
-    marginLeft: 16,
-    // padding: 8,
-    justifyContent: "center",
-  },
-  userName: {
-    fontSize: 14,
-    fontWeight: "700",
-  },
-  userEmail: {
-    fontSize: 12,
-    fontWeight: "400",
-  },
-  cont: {
-    // flex: 7,
-    backgroundColor: "#FFF",
-    width: "100%",
-    justifyContent: "center",
-    // alignItems: "center",
-  },
-  plusPic: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default PostsScreen;
