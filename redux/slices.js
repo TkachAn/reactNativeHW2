31c8c8c4885a2e889069d0c8a46d01b96f897201
unknown <4ndrey.tkachenko@gmail.com>
2023-01-30 00:15:30 +0200

    Update slices.js

--- redux/slices.js
+++ redux/slices.js
@@ -1,13 +1,15 @@
-// import {createSlice} from "@reduxjs/toolkit";
-// //
-// export const authSlice = createSlice({
-//   name: "auth",
-//   initialState: {
-//     userId: null,
-//     nickname: null,
-//   },
-//   reducers: {},
-// });
+import {createSlice} from "@reduxjs/toolkit";
+//
+console.log("Slises");
+//
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    userId: null,
+    login: null,
+  },
+  reducers: {},
+});
 // //
 // export const dashSlice = createSlice({
 //   name: "auth",
