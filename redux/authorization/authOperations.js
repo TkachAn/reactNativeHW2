c7dd4417bdd6d467d521af2a32a41cded3a6ec83
unknown <4ndrey.tkachenko@gmail.com>
2023-01-30 00:16:20 +0200

    Update authOperations.js

--- redux/authorization/authOperations.js
+++ redux/authorization/authOperations.js
@@ -1,20 +1,29 @@
-import db from "../../firebase/config";
-
+import firebase from "../../firebase/config";
+import {authSlice} from "./auth";
 //
+console.log("Operations");
+// const {updateUserProfile, authSignOut, authStateChange} = authSlice.actions;
 const authSignUp =
-  ({login, email, pass}) =>
-  async (dispatch, getSatte) => {
+  ({email, pass, login}) =>
+  async (dispatch, getState) => {
+    console.log("email, password, nickname", email, pass, login);
     try {
-      const user = await db.auth().createUserWithEmailAndPassword(email, pass); //createUserWithEmailAndPassword
+      const user = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, pass);
+      await user.updateProfile({
+        displayName: login,
+      }); //createUserWithEmailAndPassword
       console.log("user:", user);
     } catch (error) {
-      console.log("error:", error);
-      console.log("error.message:", error.message);
+      // console.log("error:", error);
+      console.log("error.code:", error.code);
+      // console.log("error.message:", error.message);
     }
   };
 //
-const authSignIn = () => async (dispatch, getSatte) => {};
+const authSignIn = () => async (dispatch, getState) => {};
 //
-const authSignOut = () => async (dispatch, getSatte) => {};
+const authSignOut = () => async (dispatch, getState) => {};
 
 export {authSignIn, authSignUp, authSignOut};
