ad9b275771440490b3b125af297bfba16aea5911
unknown <4ndrey.tkachenko@gmail.com>
2023-01-30 00:16:35 +0200

    Update auth.js

--- redux/authorization/auth.js
+++ redux/authorization/auth.js
@@ -1,10 +1,13 @@
 import {createSlice} from "@reduxjs/toolkit";
 //
+console.log("AuthReducsr");
+//
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
     userId: null,
     login: null,
+    // email: null,
   },
   reducers: {},
 });
