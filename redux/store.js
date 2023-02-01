89da20ca3f63584481c937e9929c0e2e071acc1f
unknown <4ndrey.tkachenko@gmail.com>
2023-02-01 07:38:08 +0200

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,12 @@
-// import {configureStore, combineReducers} from "@reduxjs/toolkit";
-// import {authSlice} from "./authorization/auth";
-// //
-// console.log("Store");
-// //
-// const rootReducer = combineReducers({
-//   [authSlice.name]: authSlice.reducer,
-// });
+import {configureStore, combineReducers} from "@reduxjs/toolkit";
+import {authSlice} from "./authorization/auth";
+//
+console.log("Store!");
+//
+const rootReducer = combineReducers({
+  [authSlice.name]: authSlice.reducer,
+});
 
-// export const store = configureStore({
-//   reducer: rootReducer,
-// });
+export const store = configureStore({
+  reducer: rootReducer,
+});
