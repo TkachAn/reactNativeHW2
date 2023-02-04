5e54f968f11ba5c24cf174460795726dd6fb0fd0
unknown <4ndrey.tkachenko@gmail.com>
2023-02-04 09:26:14 +0200

    Update form.jsx

--- screens/components/form.jsx
+++ screens/components/form.jsx
@@ -4,58 +4,49 @@ import {
   StyleSheet,
   Text,
   View,
-  ImageBackground,
   TextInput,
   TouchableOpacity,
   Platform,
   KeyboardAvoidingView,
   Keyboard,
-  TouchableWithoutFeedback,
 } from "react-native";
-import {authSignUp} from "../../redux/authorization/authOperations";
-import Avatar from "../components/avatar";
 //
 const initialState = {
   login: "",
   email: "",
   password: "",
-  avatar: "",
 };
 console.log("form");
-export default function Form({navigation}) {
+export default function Form({title, handleClick, login = true, email = true}) {
   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
-  const [state, setstate] = useState(initialState);
+  const [state, setState] = useState(initialState);
   const [isShowPassword, setIsShowPassword] = useState(true);
-  //
-  const dispatch = useDispatch();
   //
   const handleSubmit = () => {
     Keyboard.dismiss();
-    setstate(initialState);
+    setState(initialState);
     console.log(state);
-    if (state) {
-      dispatch(authSignUp(state));
-    }
+    handleClick(state);
     setIsShowKeyboard(false);
-    setIsShowPassword(true);
+    setIsShowPassword(false);
   };
   const showPassword = () => setIsShowPassword(!isShowPassword);
   return (
-    <View style={styles.container}>
-      <View style={styles.containerForm}>
-        <View style={styles.avatarPosition}>
-          <View style={styles.avatarBox}>
-            <Avatar />
-          </View>
-        </View>
-
-        <View style={styles.titlePosition}>
-          <Text style={styles.title}>Регистраця</Text>
-        </View>
-        <KeyboardAvoidingView
-          behavior={Platform.OS == "ios" ? "padding" : "height"}
-        >
-          <View>
+    <View style={styles.form}>
+      <View
+        style={{
+          ...styles.titlePosition,
+          marginTop: isShowKeyboard ? 10 : 30,
+          marginBottom: isShowKeyboard ? 10 : 30,
+        }}
+      >
+        <Text style={styles.title}>{title}</Text>
+      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+      >
+        <View>
+          {login && (
             <View style={styles.inputBox}>
               <TextInput
                 keyboardType="email-address"
@@ -64,10 +55,12 @@ export default function Form({navigation}) {
                 onFocus={() => setIsShowKeyboard(true)}
                 value={state.login}
                 onChangeText={(value) =>
-                  setstate((prevState) => ({...prevState, login: value}))
+                  setState((prevState) => ({...prevState, login: value}))
                 }
               />
             </View>
+          )}
+          {email && (
             <View style={styles.inputBox}>
               <TextInput
                 keyboardType="email-address"
@@ -76,74 +69,48 @@ export default function Form({navigation}) {
                 onFocus={() => setIsShowKeyboard(true)}
                 value={state.email}
                 onChangeText={(value) =>
-                  setstate((prevState) => ({...prevState, email: value}))
+                  setState((prevState) => ({...prevState, email: value}))
                 }
               />
             </View>
-            <View style={styles.inputBox}>
-              <TextInput
-                style={styles.input}
-                // keyboardType="email-address"
-                placeholder="Пароль"
-                onFocus={() => setIsShowKeyboard(true)}
-                secureTextEntry={isShowPassword}
-                value={state.password}
-                onChangeText={(value) =>
-                  setstate((prevState) => ({...prevState, password: value}))
-                }
-              />
-              <TouchableOpacity style={styles.input_btn} onPress={showPassword}>
-                <Text style={styles.input__text}>Показать</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View>
-            <TouchableOpacity
-              style={styles.btn}
-              onPress={handleSubmit}
-              activeOpacity={0.8}
-            >
-              <Text style={styles.btnTitle}>Зарегистрироваться</Text>
+          )}
+          <View style={styles.inputBox}>
+            <TextInput
+              style={styles.input}
+              // keyboardType="email-address"
+              placeholder="Пароль"
+              onFocus={() => setIsShowKeyboard(true)}
+              secureTextEntry={isShowPassword}
+              value={state.password}
+              onChangeText={(value) =>
+                setState((prevState) => ({...prevState, password: value}))
+              }
+            />
+            <TouchableOpacity style={styles.input_btn} onPress={showPassword}>
+              <Text style={styles.input__text}>Показать</Text>
             </TouchableOpacity>
           </View>
-        </KeyboardAvoidingView>
-        <View style={styles.input2}>
-          <TouchableOpacity
-            style={styles.link}
-            onPress={() => navigation.navigate("LoginScreen")}
-          >
-            <Text style={styles.link__text}>Уже есть аккаунт? Войти</Text>
-          </TouchableOpacity>
-          <View style={styles.indicator}></View>
         </View>
-      </View>
+        <View>
+          <TouchableOpacity
+            style={{...styles.btn, marginTop: isShowKeyboard ? 0 : 20}}
+            onPress={handleSubmit}
+            activeOpacity={0.8}
+          >
+            <Text style={styles.btnTitle}>ОТПРАВИТЬ</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  imageBG: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "flex-end",
-  },
-  containerForm: {
+  form: {
     backgroundColor: "#fff",
   },
-  avatarPosition: {
-    position: "relative",
-    alignItems: "center",
-  },
-  avatarBox: {
-    position: "absolute",
-    top: -60,
-  },
-
   titlePosition: {
-    marginTop: 80,
+    marginTop: 30,
     alignItems: "center",
     marginBottom: 30,
   },
@@ -153,7 +120,7 @@ const styles = StyleSheet.create({
     fontWeight: "500",
   },
   inputBox: {
-    marginBottom: 16,
+    marginBottom: 15,
     paddingRight: 18,
     paddingLeft: 18,
   },
@@ -169,9 +136,9 @@ const styles = StyleSheet.create({
   btn: {
     backgroundColor: "#FF6C00",
     height: 50,
-    borderRadius: 8, //25,
+    borderRadius: 25, //8, //25,
     marginTop: 20,
-    marginBottom: 16,
+    marginBottom: 15,
     justifyContent: "center",
     alignItems: "center",
     marginHorizontal: 20,
@@ -188,9 +155,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#212121",
     marginBottom: 7,
   },
-  input2: {
-    alignItems: "center",
-  },
+
   input_btn: {
     position: "absolute",
 
