374d1e3829b378af69203e4f85c5749cd81b147e
unknown <4ndrey.tkachenko@gmail.com>
2023-02-04 09:26:40 +0200

    Update auth.js

--- redux/authorization/auth.js
+++ redux/authorization/auth.js
@@ -6,14 +6,29 @@ export const authSlice = createSlice({
   name: "auth",
   initialState: {
     userId: null,
+    // nickName: null,
+    stateChange: null,
     login: null,
-    // email: null,
+    email: null,
   },
   reducers: {
-    // removeUser(state) {
-    //   state.email = null;
-    //   state.login = null;
-    //   state.userId = null;
+    updateUserProfile: (state, {payload}) => ({
+      ...state,
+      userId: payload.userId,
+      login: payload.login,
+      email: payload.email,
+    }),
+    authStateChange: (state, {payload}) => ({
+      ...state,
+      stateChange: payload.stateChange,
+    }),
+    authSignOut: () => initialState,
+    // updateUserProfile: (state, {payload}) => {
+    //   state.userId = payload.uid;
+    //   state.nickName = payload.displayName;
+    // },
+    // updateStatusChange: (state, {payload}) => {
+    //   state.stateChange = payload;
     // },
   },
 });
