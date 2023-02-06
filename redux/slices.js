22271ce4ba49e088074a88c3823b59b986543019
unknown <4ndrey.tkachenko@gmail.com>
2023-02-06 07:02:37 +0200

    Update slices.js

--- redux/slices.js
+++ redux/slices.js
@@ -1,25 +1,26 @@
-import {createSlice} from "@reduxjs/toolkit";
-//
-console.log("Slises!");
-//
-export const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    userId: null,
-    login: null,
-    nickName: null,
-    stateChange: null,
-  },
-  reducers: {
-    updateUserProfile: (state, {payload}) => {
-      state.userId = payload.uid;
-      state.nickName = payload.displayName;
-    },
-    updateStatusChange: (state, {payload}) => {
-      state.stateChange = payload;
-    },
-  },
-});
+// import {createSlice} from "@reduxjs/toolkit";
+// //
+// console.log("Slises!");
+// //
+// export const authSlice = createSlice({
+//   name: "auth",
+//   initialState: {
+//     userId: null,
+//     login: null,
+//     nickName: null,
+//     stateChange: null,
+//   },
+//   reducers: {
+//     updateUserProfile: (state, {payload}) => {
+//       console.log("updateUserProfile state:", state);
+//       state.userId = payload.uid;
+//       state.nickName = payload.displayName;
+//     },
+//     updateStatusChange: (state, {payload}) => {
+//       state.stateChange = payload;
+//     },
+//   },
+// });
 // // //
 // // export const dashSlice = createSlice({
 // //   name: "auth",
