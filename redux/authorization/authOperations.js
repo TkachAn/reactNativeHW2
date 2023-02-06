5ab9a03539bf046655181bf2dec73643b0380372
unknown <4ndrey.tkachenko@gmail.com>
2023-02-06 07:02:54 +0200

    Update authOperations.js

--- redux/authorization/authOperations.js
+++ redux/authorization/authOperations.js
@@ -2,10 +2,13 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   updateProfile,
+  signOut,
 } from "firebase/auth";
 //
 import {auth} from "../../firebase/config";
-import {authSlice} from "../authorization/auth";
+import {authSlice} from "./authSlice";
+//
+const {updateUserProfile, authStateChange, authLogOut} = authSlice.actions;
 //
 console.log("Operations!");
 //
@@ -20,10 +23,10 @@ const authSignUp =
       });
       const user = auth.currentUser;
       console.log("authSignUp user:", user);
-      dispatch(authSlice.actions.updateUserProfile(user));
+      dispatch(updateUserProfile(user));
     } catch (error) {
-      console.log("error:", error);
-      // console.log("error.message:", error.message);
+      // console.log("error:", error);
+      console.log("error message Up!", error.message);
     }
   };
 //
@@ -32,16 +35,51 @@ const authSignIn =
   async (dispatch, getState) => {
     try {
       const user = await signInWithEmailAndPassword(auth, email, password);
-      console.log("user", user);
+      console.log("user", auth.currentUser.displayName);
     } catch (error) {
-      console.log("error", error);
-      console.log("error.message", error.message);
+      // console.log("error", error);
+      console.log("error message In!", error.message);
     }
   };
 //
 const authSignOut = () => async (dispatch, getState) => {
-  await signOut(auth);
-  dispatch(authSignOut());
+  try {
+    await signOut(auth);
+    dispatch(authLogOut());
+    dispatch(authStateChange({stateChange: false}));
+  } catch (error) {
+    console.log("error message Out!", error.message);
+  }
 };
+const authStateCahnge = () => async (dispatch, getState) => {
+  try {
+    await onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("displayName", user.displayName);
+        const userUpdateProfile = {
+          login: user.displayName,
+          userId: user.uid,
+        };
+        dispatch(authStateChange({stateChange: true}));
+        dispatch(updateUserProfile(userUpdateProfile));
+      } else {
+        console.log("error message Chenge!", error.message);
+      }
+    });
+  } catch (error) {
+    console.log("error message Chenge!", error.message);
+  }
 
-export {authSignIn, authSignUp, authSignOut};
+  // await db.auth().onAuthStateChanged((user) => {
+  //   if (user) {
+  //     const userUpdateProfile = {
+  //       nickName: user.displayName,
+  //       userId: user.uid,
+  //     };
+
+  //     dispatch(authStateChange({stateChange: true}));
+  //     dispatch(updateUserProfile(userUpdateProfile));
+  //   }
+  //  });
+};
+export {authSignIn, authSignUp, authSignOut, authStateCahnge};
