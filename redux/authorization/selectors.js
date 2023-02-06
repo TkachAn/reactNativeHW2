f9314bb399539795cd1f733d8da21bb115ae8031
unknown <4ndrey.tkachenko@gmail.com>
2023-02-06 20:44:06 +0200

    Update selectors.js

--- redux/authorization/selectors.js
+++ redux/authorization/selectors.js
@@ -2,6 +2,7 @@
 export const getLogin = (state) => state.auth.login;
 export const getEmail = (state) => state.auth.email;
 export const getUserId = (state) => state.auth.userId;
+export const getAvatar = (state) => state.auth.avatar;
 export const getStateChange = (state) => state.auth.stateChange;
 // console.log("state", state);
 // console.log("getLogin", getLogin(Auth()));
