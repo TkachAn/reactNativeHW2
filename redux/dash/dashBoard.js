9c37220cf98c6c81973faf50c78caa774d1feedc
unknown <4ndrey.tkachenko@gmail.com>
2023-02-09 21:23:40 +0200

    Create dashBoard.js

---
+++ redux/dash/dashBoard.js
@@ -0,0 +1,11 @@
+import {createSlice} from "@reduxjs/toolkit";
+//
+export const dashSlice = createSlice({
+  name: "dashboard",
+  initialState: {
+    photoId: null,
+    name: null,
+    ref: null,
+  },
+  reducers: {},
+});
