3333e9f732cd31b16dc3b2430aa52ea9bc3ea556
unknown <4ndrey.tkachenko@gmail.com>
2023-02-09 21:24:48 +0200

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,12 @@
 import {configureStore, combineReducers} from "@reduxjs/toolkit";
 import {authSlice} from "./authorization/authSlice";
+import {photoSlice} from "./dash/photoSlices";
 //
 console.log("Store!");
 //
 const rootReducer = combineReducers({
   [authSlice.name]: authSlice.reducer,
+  [photoSlice.name]: authSlice.reducer,
 });
 
 export const store = configureStore({
