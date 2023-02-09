54389bd8943fee18473c68f5ee91725310297ae7
unknown <4ndrey.tkachenko@gmail.com>
2023-02-09 21:23:34 +0200

    Create photoSlices.js

---
+++ redux/dash/photoSlices.js
@@ -0,0 +1,32 @@
+import {createSlice} from "@reduxjs/toolkit";
+//
+console.log("Slises!");
+//
+const initialPhoto = {
+  photoId: null,
+  photo: null,
+  title: null,
+  photoLocation: null,
+  currentLocation: null,
+};
+export const photoSlice = createSlice({
+  name: "photo",
+  initialPhoto,
+  reducers: {
+    createPhoto: (state, {payload}) => {
+      state.photoId = payload.photoId;
+      state.photo = payload.photo;
+      state.title = payload.title;
+      state.photoLocation = payload.photoLocation;
+      state.currentLocation = payload.currentLocation;
+    },
+    deletePhoto: (state, {payload}) => {
+      state.photoId = null;
+      state.photo = null;
+      state.title = null;
+      state.photoLocation = null;
+      state.currentLocation = null;
+    },
+  },
+});
+
