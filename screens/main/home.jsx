a2cc12c6849314a64dd9bd31643cc0110375a3fa
unknown <4ndrey.tkachenko@gmail.com>
2023-02-10 13:43:55 +0200

    Delete home.jsx

--- screens/main/home.jsx
+++
@@ -1,7 +0,0 @@
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {useDispatch} from "react-redux";
-//
-import {authSignOut} from "../../redux/authorization/authOperations";
-import {Feather} from "@expo/vector-icons";
-import {StyleSheet, TouchableOpacity} from "react-native";
