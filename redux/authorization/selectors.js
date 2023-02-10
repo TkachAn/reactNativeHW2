7a275e941cbe312f93b54c90a0435b48bc67d6e2
unknown <4ndrey.tkachenko@gmail.com>
2023-02-10 13:45:02 +0200

    Update selectors.js

--- redux/authorization/selectors.js
+++ redux/authorization/selectors.js
@@ -1,8 +1,5 @@
-// import {Auth} from "firebase/auth";
 export const getLogin = (state) => state.auth.login;
 export const getEmail = (state) => state.auth.email;
 export const getUserId = (state) => state.auth.userId;
 export const getAvatar = (state) => state.auth.avatar;
 export const getStateChange = (state) => state.auth.stateChange;
-// console.log("state", state);
-// console.log("getLogin", getLogin(Auth()));
